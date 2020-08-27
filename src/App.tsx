0623c2fe0af7ffa1bb05e9b68d1537cc6a32e0fc
pparkddo <composuer_p@naver.com>
2020-08-27 18:07:31 +0900

    Feat: Add react-boostrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React
-        </p>
-      </header>
-    </div>
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col md="auto">
+          <Button variant="primary" onClick={() => alert("Hello")}>
+            Click Me !
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
